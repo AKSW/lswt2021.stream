bb6b01b86ecf983f10f534116037879af0fae0d2
Natanael Arndt <natanael.arndt@eccenca.com>
2022-07-01 12:27:39 +0200

    Update packages to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    main: './src',
+  mode: 'production', // development production
+  plugins: [new MiniCssExtractPlugin()],
+  output: {
+    path: __dirname + '/_includes/dist',
   },
   module: {
     rules: [
       {
-        test: /.scss$/,
-        use: ExtractTextPlugin.extract({
-          publicPath: '../../',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
+        test: /\.s[ac]ss$/i,
+        use: [
+          // https://webpack.js.org/plugins/mini-css-extract-plugin/
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
   },
-  plugins: [new ExtractTextPlugin({
-    filename: '[name].css',
-    allChunks: true,
-  })]
-}
+};
