d820c758ccc4c4bf4bfc1a43a3246fd50d394d80
Natanael Arndt <natanael.arndt@eccenca.com>
2023-06-20 18:12:39 +0200

    updated chat

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { io } from "socket.io-client";
+//const socket = io("wss://chat.lswt2021.comiles.eu");
+// const ws = new WebSocket('wss://chat.lswt2021.comiles.eu/ws');
+const ws = new WebSocket('ws://localhost:5000/ws');
 
-const socket = io("wss://chat.lswt2021.comiles.eu");
-// const socket = io("ws://localhost:8000");
-
-socket.on('server_message', (data) => {
+ws.addEventListener('message', function (event) {
+  console.log("message received");
+  console.log(event);
+  data = JSON.parse(event.data)
+  console.log(data);
   let e = document.createElement('p');
   let sp_nick = document.createElement('span');
   let sp_time = document.createElement('span');
@@ -21,20 +24,37 @@ socket.on('server_message', (data) => {
   document.getElementById('message-box').prepend(e);
 });
 
-function htmlEntities(str) {
-  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+function sendMessage() {
+  console.log("send message");
+
+  const nickname = htmlEntities(document.getElementById('nickname-input').value);
+  const message_body = htmlEntities(document.getElementById('message-input').value);
+
+  if (nickname.trim() == "" || message_body.trim() == "") {
+    console.log("nickname or message body not set, will not send message");
+    if (nickname.trim() == "") {
+      document.getElementById('nickname-input').required = "true";
+    }
+    if (message_body.trim() == "") {
+      document.getElementById('message-input').required = "true";
+    }
+  } else {
+    const message = {
+      body: message_body,
+      nickname: nickname
+    };
+    console.log(message);
+    if (message) {
+      ws.send(JSON.stringify(message));
+    }
+    document.getElementById('message-input').value = '';
+    return false;
+  }
 }
 
-function sendMessage(){
-  if (document.getElementById('nickname-input').value.trim() == "") {
-    document.getElementById('nickname-input').required = "true";
-  } else {
-    socket.emit('client_message', {
-      'nickname': htmlEntities(document.getElementById('nickname-input').value),
-      'message': htmlEntities(document.getElementById('message-input').value)
-    });
-    document.getElementById('message-input').value = '';
-}
+
+function htmlEntities(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
 function isPressingEnter(e){
