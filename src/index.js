cfd67e9db63864fbe6fc86822a73b38c09bdf0e4
Natanael Arndt <natanael.arndt@eccenca.com>
2023-06-23 00:55:23 +0200

    works

--- src/index.js
+++ src/index.js
@@ -1,29 +1,79 @@
 import { io } from "socket.io-client";
 
 // const socket = io("wss://chat.lswt2021.comiles.eu/ws");
-const socket = io("ws://localhost:5000/ws");
+const socket = io("ws://localhost:8000", {someVar: "bla"});
 // const ws = new WebSocket('wss://chat.lswt2021.comiles.eu/ws');
 // const ws = new WebSocket('ws://localhost:5000/ws');
 
-socket.on('server_message', (data) => {
+socket.on('connect', () => {
+  console.log(`connect ${socket.id}`);
+  var stream_id = window.location.hash.substring(1);
+  zapp(stream_id);
+});
+
+socket.on('disconnect', () => {
+  console.log(`disconnect ${socket.id}`);
+});
+
+socket.on('server_message', (message) => {
   console.log("message received");
-  console.log(data);
+  console.log(message);
   let e = document.createElement('p');
-  let sp_nick = document.createElement('span');
-  let sp_time = document.createElement('span');
-  let sp_message = document.createElement('span');
-  sp_nick.innerHTML = data.nickname;
-  sp_time.innerHTML = data.time;
-  sp_time.setAttribute("class", "date");
-  sp_message.innerHTML = data.body;
-  e.append(sp_time);
-  e.append(" ");
-  e.append(sp_nick);
-  e.append(": ");
-  e.append(sp_message);
+  if (message.type == 'message') {
+    let sp_nick = document.createElement('span');
+    let sp_time = document.createElement('span');
+    let sp_message = document.createElement('span');
+    sp_nick.innerHTML = message.nickname;
+    sp_time.innerHTML = message.time;
+    sp_time.setAttribute("class", "date");
+    sp_message.innerHTML = message.body;
+    e.append(sp_time);
+    e.append(" ");
+    e.append(sp_nick);
+    e.append(": ");
+    e.append(sp_message);
+  }
+  if (message.type == 'topic') {
+    let sp_nick = document.createElement('span');
+    let sp_message = document.createElement('span');
+    sp_nick.innerHTML = "topic";
+    sp_message.innerHTML = message.body;
+    e.append(sp_nick);
+    e.append(": ");
+    e.append(sp_message);
+    updateVideo(message.body);
+    // updateStreams(topicData.streams);
+  }
 
   document.getElementById('message-box').prepend(e);
 });
+
+function updateVideo(videoUrl) {
+  let currentUrl = document.getElementById('videoframe').getAttribute("src");
+  if (currentUrl != videoUrl) {
+    document.getElementById('videoframe').setAttribute("src", videoUrl);
+  }
+}
+
+socket.on('streams_update', (streamList) => {
+  const stream_selection = document.getElementById('stream_selection');
+  stream_selection.innerHTML = '';
+  console.log(streamList);
+  for (let room_id in streamList) {
+    let stream_link = document.createElement('a');
+    stream_link.setAttribute("href", "javascript:zapp('" + room_id + "')");
+    stream_link.append(streamList[room_id] );
+    stream_selection.append(stream_link);
+    stream_selection.append(" ");
+  }
+});
+
+function zapp(stream_id) {
+  // clear messages
+  const message_box = document.getElementById('message-box');
+  message_box.innerHTML = '';
+  socket.emit('client_zapp', stream_id);
+}
 
 function sendMessage() {
   console.log("send message");
@@ -75,3 +125,4 @@ function isPressingEnter(e){
 
 window.isPressingEnter = isPressingEnter;
 window.sendMessage = sendMessage;
+window.zapp = zapp;
